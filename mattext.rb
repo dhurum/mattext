4fac64f939cb11ca41c9e132652bd2fc06e2b3e4
Denis Tikhomirov <dvtikhomirov@gmail.com>
2014-02-01 22:33:35 +0400

    Modified brew formula for version 0.7.1

--- mattext.rb
+++ mattext.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Mattext < Formula
   homepage 'https://github.com/dhurum/mattext'
   url 'https://github.com/dhurum/mattext/archive/v0.7.tar.gz'
-  sha1 '79393ffabc7d3cbeb2fa02f35ea9474f3b92f55ccae8a51a80721902b27472db'
+  sha1 'dc49b8cff889a9575334f303110dbb16053af00bfbd671fc75b32937be95acd7'
   depends_on 'cmake' => :build
   depends_on 'argp-standalone'
 
