65982819fba3cd2345999647ba562507cc049f96
Denis Tikhomirov <dvtikhomirov@gmail.com>
2015-10-02 21:20:34 +0300

    Use GPLv3

--- src/manager_interactive.cpp
+++ src/manager_interactive.cpp
@@ -1,3 +1,24 @@
+/*******************************************************************************
+
+Copyright 2015 Denis Tikhomirov
+
+This file is part of Mattext
+
+Mattext is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Mattext is distributed in the hope that it will be useful,
+     but WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     GNU General Public License for more details.
+
+     You should have received a copy of the GNU General Public License
+     along with Mattext.  If not, see http://www.gnu.org/licenses/.
+
+*******************************************************************************/
+
 #include <fcntl.h>
 #include <unistd.h>
 #include <sstream>
